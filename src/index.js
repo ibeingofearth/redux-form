d2077b167e04c1a7b8a58d7a455a793ffc1be8b2
RMVivek <vivekrm@tridentnets.in>
2022-10-07 15:33:54 +0530

    comit

--- src/index.js
+++ src/index.js
@@ -10,3 +10,15 @@ root.render(
     <App/>
   </Provider>,
 );
+//routing pages in react?
+// {/* <Router history={browserHistory}> */}
+  // <Route path='/' component={App} >
+    // <IndexRoute component={Home} />
+    // <Route path='product/:productId' component={Product} />
+    // <Route path='category/:catNumber' component={Category} />
+    // <Route path='cart' component={Cart} />
+  // </Route>
+  // <Route path='*' component={NotFound} />
+// </Router>
+
+
